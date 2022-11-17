338f34f38b9a96ff2db39c77147c239bb8ff5229
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2022-11-17 18:00:24 +0100

    [app.vue-out-of-src]mistake

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
-import AppHeader from './src/components/AppHeader.vue'
-import AppMain from './src/components/AppMain.vue'
-import CharactersList from './src/components/CharactersList.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import CharactersList from './components/CharactersList.vue'
 
 export default {
   name:'App',
